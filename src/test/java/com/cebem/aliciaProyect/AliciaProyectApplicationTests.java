b3f121507a294553aec765d9a9e962f0065bdd5b
APerezVila <perezvilaalicia@gmail.com>
2021-05-25 22:11:01 +0200

    Subida Base de Proyecto

---
+++ src/test/java/com/cebem/aliciaProyect/AliciaProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebem.aliciaProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliciaProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
